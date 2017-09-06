df3352cef4364ee091751879ff9ccdf81ffdd421
JGeraldoLima <j.gbaixinho@gmail.com>
2017-09-06 00:29:42 -0300

    Reorganized files / First Menu UI Version

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,94 @@
+﻿using UnityEngine;
+
+// Include the namespace required to use Unity UI
+using UnityEngine.UI;
+
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public Text countText;
+    public Text winText;
+
+    private Rigidbody rb;
+    private int count;
+
+    void Main()
+    {
+        // Preventing mobile devices going in to sleep mode 
+        //(actual problem if only accelerometer input is used)
+        Screen.sleepTimeout = SleepTimeout.NeverSleep;
+    }
+
+    // At the start of the game..
+    void Start()
+    {
+        // Assign the Rigidbody component to our private rb variable
+        rb = GetComponent<Rigidbody>();
+
+        // Set the count to zero 
+        count = 0;
+
+        // Run the SetCountText function to update the UI (see below)
+        SetCountText();
+
+        // Set the text property of our Win Text UI to an empty string, making the 'You Win' (game over message) blank
+        winText.text = "";
+    }
+
+    // Each physics step..
+    void FixedUpdate()
+    {
+        Vector3 dir = Vector3.zero;
+        dir.x = Input.acceleration.x;
+        dir.z = -Input.acceleration.z;
+        if (dir.sqrMagnitude > 1)
+            dir.Normalize();
+
+        dir *= Time.deltaTime * speed;
+        rb.transform.Translate(dir);
+    }
+
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+
+    // When this game object intersects a collider with 'is trigger' checked, 
+    // store a reference to that collider in a variable named 'other'..
+    void OnTriggerEnter(Collider other)
+    {
+        // ..and if the game object we intersect has the tag 'Pick Up' assigned to it..
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            // Make the other game object (the pick up) inactive, to make it disappear
+            other.gameObject.SetActive(false);
+
+            // Add one to the score variable 'count'
+            count = count + 1;
+
+            // Run the 'SetCountText()' function (see below)
+            SetCountText();
+        }
+    }
+
+    // Create a standalone function that can update the 'countText' UI and check if the required amount to win has been achieved
+    void SetCountText()
+    {
+        // Update the text field of our 'countText' variable
+        countText.text = "Count: " + count.ToString();
+
+        // Check if our 'count' is equal to or exceeded 12
+        if (count >= 12)
+        {
+            // Set the text value of our 'winText'
+            winText.text = "You Win!";
+        }
+    }
+
+}
