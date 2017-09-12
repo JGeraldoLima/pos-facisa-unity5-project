86d913b5ca85e485e1c1d255d818778dcac2e886
JGeraldoLima <j.gbaixinho@gmail.com>
2017-09-12 01:25:23 -0300

    Finished functional main menu prototype with working Start game button

---
+++ Assets/Scripts/LoadGameOnClick.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadGameOnClick : MonoBehaviour
+{
+
+    public void LoadSceneByIndex(int index)
+    {
+		/* NOT WORKING, MAYBE THERE SOMETHING TRANSPARENT OVER THE BUTTONS */
+		Debug.Log("LOADING");
+		SceneManager.LoadScene(index);
+    }
+}
